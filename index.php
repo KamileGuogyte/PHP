f6eacc4978e94ebd5b38f8bb2610bc57cfe6eaa3
KamileGuogyte <kamile.guogyte@gmail.com>
2019-09-09 16:53:53 +0300

    First commit

---
+++ index.php
@@ -0,0 +1,21 @@
+
+<!DOCTYPE html>
+<html>
+    <head>
+         <meta charset="UTF-8">
+        <title>Aš valdau HTML ir PHP!</title>
+    </head>
+    <body>
+        <h1>Kamilė - HTML ir PHP asė!</h1>
+        <p>Kuriu dinaminį turinį.
+        Atidaryta: 
+        
+<?php
+print "Today is " . date("Y/m/d") . "<br>";
+print "The time is " . date("h:i:sa");
+     ?>   
+        </p>
+    </body>
+</html>
+
+
