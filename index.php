e49d5a00195adec990c5026a6f241b10ca161098
KamileGuogyte <kamile.guogyte@gmail.com>
2019-09-10 16:52:55 +0300

    Laikas ir data

--- index.php
+++ index.php
@@ -1,19 +1,22 @@
 <?php
-// **
-// Komentarai
-// *
 
-print 'Labas';
-print 'Iki';
+date_default_timezone_set('Europe/Vilnius');
+print date('Y-M-d H:i:sa');
+print date(' Y-m-d', strtotime('+1 week'));
+        
+
 ?>
 
+
+<!DOCTYPE html>
 <html>
     <head>
-        <title>Puslapio pavadinimas</title>    
+         <meta charset="UTF-8">
+        <title>PHP lydės ir <?php print date(' Y-m-d', strtotime('+1 day')); ?></title>
     </head>
     <body>
-        <h1 ghgh </h1>
+        <h1>Kamilė - PHP su manim buvo ir <?php print date(' H:i:s', strtotime('-1 hour')) . ' valandą!';?></h1>
+        <p><?php print date('Y', strtotime('+1 year')); ?> ne už kalnų!</p>
+       
     </body>
-
 </html>
-
