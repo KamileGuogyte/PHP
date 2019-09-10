d0d35acd8ef84db16c15062328cf6981ab1a9d54
KamileGuogyte <kamile.guogyte@gmail.com>
2019-09-10 15:27:50 +0300

    Komentarai

--- index.php
+++ index.php
@@ -1,14 +1,19 @@
+<?php
+// **
+// Komentarai
+// *
 
-<!DOCTYPE html>
+print 'Labas';
+print 'Iki';
+?>
+
 <html>
     <head>
-         <meta charset="UTF-8">
-        <title><?php print 'Aš , '. date("l") .' ir PHP!';?> </title>
+        <title>Puslapio pavadinimas</title>    
     </head>
     <body>
-        <h1>Kamilė - HTML <?php print 'ir PHP';?> asė! <br> jau nuo <?php print date("Y"); ?> metų</h1>
-        <p>Viskas prasidėjo <?php print date("m") .'<br>' .'mėnesio ' . date("d") .' dieną';?>!</p>
-       
+        <h1 ghgh </h1>
     </body>
+
 </html>
 
