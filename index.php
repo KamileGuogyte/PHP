561a6ea8805a233c89752a43d18ef69103f0f2ea
KamileGuogyte <kamile.guogyte@gmail.com>
2019-09-11 11:47:30 +0300

    Randoma laikai

--- index.php
+++ index.php
@@ -1,22 +1,12 @@
-<?php
-
-date_default_timezone_set('Europe/Vilnius');
-print date('Y-M-d H:i:sa');
-print date(' Y-m-d', strtotime('+1 week'));
-        
-
-?>
-
-
 <!DOCTYPE html>
 <html>
     <head>
          <meta charset="UTF-8">
-        <title>PHP lydės ir <?php print date(' Y-m-d', strtotime('+1 day')); ?></title>
+           <title>PHP lydės ir <?php print date(' Y-m-d', strtotime('+' . rand(1, 3652) . 'days')); ?></title>
     </head>
-    <body>
-        <h1>Kamilė - PHP su manim buvo ir <?php print date(' H:i:s', strtotime('-1 hour')) . ' valandą!';?></h1>
-        <p><?php print date('Y', strtotime('+1 year')); ?> ne už kalnų!</p>
+     <body>
+        <h1>Kamilė - Galbūt turėsiu <?php print rand(1, 5); ?> vaikų! </h1>
+        <p> Trumpas nebebus prezidentu <?php print date(' Y-m-d', strtotime('+' . rand(2, 10) . 'year')); ?></p>
        
     </body>
 </html>
