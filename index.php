618bf17aa4b432f711f112ea12989a38583b65ae
KamileGuogyte <kamile.guogyte@gmail.com>
2019-09-11 13:35:31 +0300

    Bomba2

--- index.php
+++ index.php
@@ -2,15 +2,18 @@
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Bomb img game, and date()</title>
         <style>
-            img {height: 1<?php print date('s'); ?>px}
+            .bomb-img {
+                background-image: url(http://pngimg.com/uploads/bomb/bomb_PNG16.png);
+                background-size: cover;
+                width: 1<?php print date('s'); ?>px;
+                height: 1<?php print date('s'); ?>px;
+            }
         </style>
     </head>
-    <body>     
-        <img src="https://img.freepik.com/free-vector/realistic-bomb-illustration_1284-11507.jpg?size=626&ext=jpg">
-        <h1><?php print date('s'); ?></h1>
+    <body>
+        <div class="bomb-img"></div>
+        <div><?php print date('s'); ?></div>
     </body>
 </html>
-
-
