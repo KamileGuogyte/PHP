f353025118d7f63e81228570d94c49e3a9fcce42
KamileGuogyte <kamile.guogyte@gmail.com>
2019-09-11 15:26:30 +0300

    Bombos sprogimas

--- index.php
+++ index.php
@@ -4,16 +4,25 @@
         <meta charset="UTF-8">
         <title>Bomb img game, and date()</title>
         <style>
-            .bomb-img {
+            .bomb-imgA {
                 background-image: url(http://pngimg.com/uploads/bomb/bomb_PNG16.png);
                 background-size: cover;
-                width: 1<?php print date('s'); ?>px;
-                height: 1<?php print date('s'); ?>px;
+                width: 1<?php print date('s') * 1.5; ?>px;
+                height: 1<?php print date('s') * 1.5; ?>px;
+            }
+            .bomb-imgB00 {
+                background-image: url(https://cdn4.iconfinder.com/data/icons/explosion/512/as_897-512.png);
+                background-size: cover;
+                width: 100px;
+                height: 100px;
             }
         </style>
     </head>
     <body>
-        <div class="bomb-img"></div>
-        <div><?php print date('s'); ?></div>
+        <div class="bomb-imgA bomb-imgB<?php print date('s'); ?>"></div>
+         <div><?php print date('s'); ?></div>
+       
+       
     </body>
 </html>
+
