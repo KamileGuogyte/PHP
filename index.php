7587209a4cf50cd3aebb3b6b4fc77e1182ba4e85
KamileGuogyte <kamile.guogyte@gmail.com>
2019-09-12 10:32:48 +0300

    Paveikslai 4 teisingai

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <?php
+
 $img = 'https://media.mnn.com/assets/images/2015/08/union-wood-sunrise.jpg.653x0_q80_crop-smart.jpg'
+        
 ?> 
-<!DOCTYPE html>
+
 <html>
     <head>
         <meta charset="UTF-8">
         <title>Antraštė</title>
     </head>
     <body>
-        <img src='<?php print $img ?>'>
-        <img src='<?php print $img ?>'>
-        <img src='<?php print $img ?>'>
-        <img src='<?php print $img ?>'>
+        
+        <img src="<?php print $img ?>">
+        <img src="<?php print $img ?>">
+        <img src="<?php print $img ?>">
+        <img src="<?php print $img ?>">
     </body>
 </html>
 
