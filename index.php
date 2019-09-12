058aca3acc85cea14c499ac27849ee7a456b1e1a
KamileGuogyte <kamile.guogyte@gmail.com>
2019-09-12 14:29:03 +0300

    Modulis

--- index.php
+++ index.php
@@ -1,25 +1,13 @@
 <?php
 
-$siukslines_turis = 40;
-$siuksliu_turis_per_d = 15;
-$max_kaupo_turis = rand(1, 10);
-$siuksliu_turis_max = $siukslines_turis+$max_kaupo_turis;
-$siuksles_po_dienu = $siuksliu_turis_max/$siuksliu_turis_per_d;
-$suapvalintas_sk_siuksliu = floor($siuksles_po_dienu);
-$date = date('Y-m-d', strtotime('+' . ($suapvalintas_sk_siuksliu) .  'days'));
-$text = " po $suapvalintas_sk_siuksliu dienų $date pirk gėlių ir šampano, jei nori išvengti konflikto.";
+$laikas = date('s');
+$number_1 = 15;
+$number_2 = 97;
 
+$number_3 = $number_1 % $number_2;
+print $laikas . '<br/>';
+print $laikas % 2;
 ?> 
-<html>
-    <head>
-        <meta charset="UTF-8">
-    </head>
-    <body>
-    <h1> 
-        <?php print $text ?>
-    </h1>
-</body>
-</html>
 
 
   
