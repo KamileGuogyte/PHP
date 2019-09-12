3d543a161d321e0fd65310a3d2f3f7e4b89fadb7
KamileGuogyte <kamile.guogyte@gmail.com>
2019-09-12 09:50:28 +0300

    Variable

--- index.php
+++ index.php
@@ -1,21 +1,27 @@
+
 <!DOCTYPE html>
 <html>
     <head>
-         <meta charset="UTF-8">
-        <title>PHP lydės ir <?php print date('Y-m-d', strtotime('+' . rand(1, 10) . 'year' . '+' . rand(1, 12) . 'month' . '+'. rand(1, 28) .'day')); ?></title>
+        <meta charset="UTF-8">
+        <title>P></title>
     </head>
     <body>
-        <h1>
-            Kamilė - Galbūt turėsiu <?php print rand(1, 5); ?> vaikus
-        </h1>
-        <p>
-           Trumpas nebebus prezidentu <?php print date('Y-m-d', strtotime('+'. rand(2, 10). 'year')); ?>
-        </p>
-       
+        <ul>
+            <li>
+                <?php print 'Kažkoks tekstas' ?>
+            </li> 
+            <li>
+                <?php print 123 ?>
+            </li>
+            <li>
+                <?php print true ?> 
+            </li> 
+            <li>
+                <?php print (null) ?>
+            </li> 
+        </ul>
     </body>
 </html>
 
-
-
         
-     
+  
