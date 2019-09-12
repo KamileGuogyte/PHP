c43f22f2828e068f4c2b201279635edf237589d7
KamileGuogyte <kamile.guogyte@gmail.com>
2019-09-12 12:47:35 +0300

    Siuksles6

--- index.php
+++ index.php
@@ -4,10 +4,10 @@ $siukslines_turis = 40;
 $siuksliu_turis_per_d = 15;
 $max_kaupo_turis = rand(1, 10);
 $siuksliu_turis_max = $siukslines_turis+$max_kaupo_turis;
-$siuksles_po_dienu = $siuksliu_turis_max/ $siuksliu_turis_per_d;
+$siuksles_po_dienu = $siuksliu_turis_max/$siuksliu_turis_per_d;
 $suapvalintas_sk_siuksliu = floor($siuksles_po_dienu);
 $date = date('Y-m-d', strtotime('+' . ($suapvalintas_sk_siuksliu) .  'days'));
-$text = " po $suapvalintas_sk_siuksliu dienų ($date) pirk gėlių ir šampano, jei nori išvengti konflikto "
+$text = " po $suapvalintas_sk_siuksliu dienų $date pirk gėlių ir šampano, jei nori išvengti konflikto.";
 
 ?> 
 <html>
