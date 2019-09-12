cff8e2c2e8b133a62ad3a0439162ec2099bcd7e9
KamileGuogyte <kamile.guogyte@gmail.com>
2019-09-12 11:55:36 +0300

    Siuksles

--- index.php
+++ index.php
@@ -1,23 +1,21 @@
 <?php
 
-$pinigaiStart = (rand(1, 100));
-$pinigaiBack1 = (rand(101, 200));
-$pinigaiBack2 = (rand(201, 300));
+$siukslines_turis = 40;
+$siuksliu_turis_per_d = 15;
+$max_kaupo_turis = rand(1, 10);
+$rezultatas = $siukslines_turis/$siuksliu_turis_per_d+$max_kaupo_turis
 
 ?> 
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Antraštė</title>
     </head>
     <body>
-        <div class = "body1"
-            <h1>Skolos skaičiuoklė</h1>
-            <h3>Jei paėmei <?php print $pinigaiStart; ?> euru</h3>
-            <h3>Su dviem kabančiais grąžinsi <?php print $pinigaiBack1; ?></h3>
-            <h3>Su dviem kabančiais grąžinsi <?php print $pinigaiBack2; ?></h3>
-        </div    
-    </body>
+    <h1> 
+        <?php print $rezultatas ?>
+    </h1>
+   
+</body>
 </html>
 
 
