963297aee2329dbcd800f289dac684da6c5fee4c
KamileGuogyte <kamile.guogyte@gmail.com>
2019-09-12 16:12:51 +0300

    Bombos sprogimas su kintamaisiais

--- index.php
+++ index.php
@@ -1,13 +1,46 @@
 <?php
+//$laikas = date('s');
+//print $laikas . '<br>';
+//
+//$laikas_reversed = date('s', strtotime('-1 sec')); 
+//print $laikas_reversed;
 
 $laikas = date('s');
-$number_1 = 15;
-$number_2 = 97;
+//print $laikas . '<br>';
 
-$number_3 = $number_1 % $number_2;
-print $laikas . '<br/>';
-print $laikas % 2;
-?> 
+$laikas_reversed = 59 - $laikas;
+//print $laikas_reversed;
+?>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Bombos sprogimas su kintamaisiais</title>
+        <style>
+            .squareA {
+                width: 150px;
+                height: 150px;
+                background-color: pink;
+                border:solid black 5px; 
+            }
+
+            .squareB0 {
+                background-color: red;
+                border:solid yellow 5px;
+            }   
+        </style>
+    </head>
+    <body>
+        <div class="squareA squareB<?php print $laikas_reversed; ?>">
+            <p><?php print $laikas; ?></p>
+            <p><?php print $laikas_reversed; ?></p>
+        </div>
+    </body>
+</html>
 
 
-  
+
+
+
+
+
