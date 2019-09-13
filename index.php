2de4a1c2a9e2022cada77c1768ae711c7e183e36
KamileGuogyte <kamile.guogyte@gmail.com>
2019-09-13 10:07:01 +0300

    Keliones skaiciuokle

--- index.php
+++ index.php
@@ -1,46 +1,30 @@
 <?php
-//$laikas = date('s');
-//print $laikas . '<br>';
-//
-//$laikas_reversed = date('s', strtotime('-1 sec')); 
-//print $laikas_reversed;
 
-$laikas = date('s');
-//print $laikas . '<br>';
+//km
+$distance = rand(1, 10); 
 
-$laikas_reversed = 59 - $laikas;
-//print $laikas_reversed;
+//1/100km
+$consumption = 7.5; 
+
+//Eur/1
+$price_1 = 1.3;
+$fuel_total = ($distance * $consumption)/100;
+$price_trip = $fuel_total * $price_1;
+       
 ?>
-
 <html>
     <head>
         <meta charset="UTF-8">
-        <title>Bombos sprogimas su kintamaisiais</title>
-        <style>
-            .squareA {
-                width: 150px;
-                height: 150px;
-                background-color: pink;
-                border:solid black 5px; 
-            }
-
-            .squareB0 {
-                background-color: red;
-                border:solid yellow 5px;
-            }   
-        </style>
     </head>
     <body>
-        <div class="squareA squareB<?php print $laikas_reversed; ?>">
-            <p><?php print $laikas; ?></p>
-            <p><?php print $laikas_reversed; ?></p>
-        </div>
+        <h1>Kelionės skaičiuoklė</h1>
+        <ul>
+            <li> Nuvažiuota distancija <?php print $distance; ?>     
+            </li>
+          <li> Sunaudota <?php print $fuel_total; ?>
+            </li>
+           <li> Kaina <?php print $price_trip; ?> eur
+            </li>
+        </ul>
     </body>
 </html>
-
-
-
-
-
-
-
