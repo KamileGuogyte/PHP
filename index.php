8840a5cf1880aec79f4f902e105f0defdf77efb8
KamileGuogyte <kamile.guogyte@gmail.com>
2019-09-16 09:25:23 +0300

    Taisytas namu darbas if else orai

--- index.php
+++ index.php
@@ -22,30 +22,27 @@ if ($sunny && $rainy) {
         <meta charset="UTF-8">
         <title>if, else if, else </title>
         <style>
-            div {
+            .icon {
                 display: inline-block;
                 height: 300px;
-                width: 300px;
+                width: 300px; 
+                background-size: contain;                
             }
-
+            
             .sunny-rainy {
-                background: url(https://cdn2.vectorstock.com/i/1000x1000/66/26/sun-holding-cloud-rainbow-set-rain-drop-weather-vector-22876626.jpg); 
-                background-size: cover;
+                background-image: url(https://cdn2.vectorstock.com/i/1000x1000/66/26/sun-holding-cloud-rainbow-set-rain-drop-weather-vector-22876626.jpg); 
             }
 
             .sunny {
-                background: url(https://i.pinimg.com/originals/25/e9/45/25e945cbe48792dbc8ba4e9ef51cf1e2.jpg); 
-                background-size: cover;
+                background-image: url(https://i.pinimg.com/originals/25/e9/45/25e945cbe48792dbc8ba4e9ef51cf1e2.jpg); 
             }
 
             .rainy {
-                background: url(https://us.123rf.com/450wm/yupiramos/yupiramos1802/yupiramos180225830/96070237-stock-vector-cute-cartoon-happy-cloud-rain-drops-vector-illustration.jpg?ver=6);
-                background-size: cover;
+                background-image: url(https://us.123rf.com/450wm/yupiramos/yupiramos1802/yupiramos180225830/96070237-stock-vector-cute-cartoon-happy-cloud-rain-drops-vector-illustration.jpg?ver=6);
             }
 
             .cloudy {
-                background: url(https://i.pinimg.com/originals/e3/17/19/e317197d99b7b6b5b1688df45e206dcc.jpg);
-                background-size: cover;
+                background-image: url(https://i.pinimg.com/originals/e3/17/19/e317197d99b7b6b5b1688df45e206dcc.jpg);
             }
 
             .text-position {
@@ -54,7 +51,7 @@ if ($sunny && $rainy) {
         </style>
     </head>
     <body>
-        <div class="<?php print $weather; ?>"></div>
+        <div class="icon <?php print $weather; ?>"></div>
         <div class="text-position"><?php print $weather_title; ?></div>
     </body>
 </html>
