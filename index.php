58e28ced79c12f283a28a3dc6dbeca8178239273
KamileGuogyte <kamile.guogyte@gmail.com>
2019-09-16 10:12:31 +0300

    Foreach pirmasis kitas budas

--- index.php
+++ index.php
@@ -1,20 +1,10 @@
+
 <?php
 
-$x = rand(1, 10);
 
-?>
 
-<html>
-    <head>
-        <meta charset="UTF-8">
-        <title>Foreach ciklas</title>
-    </head>
-    <body>
-<?php for ($i = 1; $i <= $x; $i++):
-     ?> 
-        <p><?php print "Tai yra $i-tasis ciklas"; ?></p>
-        <?php endfor; ?>
+for ($x = 1; $x <= rand(1, 5); $x++) {
 
-    </body>
-</html>
+    print "Tai yra $x-tasis ciklas <br>";
 
+}
