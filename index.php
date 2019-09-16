dcb42bf3e7ceb6edf6ae423cbdaa9adf16f748c6
KamileGuogyte <kamile.guogyte@gmail.com>
2019-09-16 15:39:26 +0300

    Katasunis for if rand

--- index.php
+++ index.php
@@ -1,10 +1,40 @@
-
 <?php
 
+$kates = rand(1, 3);
+print "kates $kates <br>";
 
+$sunys = rand(1, 3);
+print "sunys $sunys <br>";
 
-for ($x = 1; $x <= rand(1, 5); $x++) {
+$pavyko = rand(0, 1);
 
-    print "Tai yra $x-tasis ciklas <br>";
+$katasuniai = 0;
 
-}
+for ($i = 1; $i <= $kates; $i++) {
+	print "<br/>Kačių ciklas $i <br/>";
+    for ($b = 1; $b <= $sunys; $b++) {
+		print "šunų ciklas $b <br/>";
+//		for ($j = 1; $j <= $peles; $j++) {
+//			print "pelių ciklas $j <br/>";
+		}
+        $pavyko = rand(0, 1);
+        print "randomizuotas skaicius $pavyko <br />";
+        if ($pavyko) {
+            $katasuniai++;
+            break;
+        }
+    }
+
+$h2_text = "Dalyvavo $kates kates ir $sunys sunys";
+$h3_text = "Katasuniu iseiga: $katasuniai";
+?>
+<html>
+    <head>
+        <meta charset="utf-8">
+    </head>
+    <body>
+        <h1>Katasuniu iseiga</h1>
+        <h2><?php print $h2_text; ?></h2>
+        <h3><?php print $h3_text; ?></h3>
+    </body>
+</html>
