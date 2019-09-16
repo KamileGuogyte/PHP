cd322898ce7588fe78db87c30c2c15bd1797fecc
KamileGuogyte <kamile.guogyte@gmail.com>
2019-09-16 22:53:44 +0300

    For ciklas dūmai

--- index.php
+++ index.php
@@ -1,40 +1,50 @@
 <?php
 
-$kates = rand(1, 3);
-print "kates $kates <br>";
+$days = 365;
+$cigs_mon_fri = rand(3, 4);
+$cigs_sat = rand(10, 20);
+$cigs_sun = rand(1, 3);
+$pack_price = 3.50;
 
-$sunys = rand(1, 3);
-print "sunys $sunys <br>";
+$count_ttl = 0;
+$price_ttl = 0;
 
-$pavyko = rand(0, 1);
+for ($i = 0; $i < $days; $i++) {
+//    print $i;
+    $savaites_diena = date('N', strtotime("+$i days"));
+//print $savaites_diena<br>"
 
-$katasuniai = 0;
+    if ($savaites_diena < 6) {
+        $cigs_mon_fri = rand(3, 4);
+        $count_ttl += $cigs_mon_fri;
+//        print "$savaites_diena-ieni surukiau $cigs_mon_fri<br>";
+    } elseif ($savaites_diena == 6) {
+        $cigs_sat = rand(10, 20);
+        $count_ttl += $cigs_sat;
+//        print "$savaites_diena-ieni surukiau $cigs_sat<br>";
+    } else
+        $cigs_sun = rand(1, 3);
+    $count_ttl += $cigs_sun;
+//    print "$savaites_diena-ieni surukiau $cigs_sun<br>";
+}
+print ($count_ttl/20) . "<br>";
+$ttl_packs = ceil($count_ttl/20);
+print "$ttl_packs";
+$ttl_price = $ttl_packs * $pack_price;
+print "$ttl_price";
+$text = "Per $days surukysiu $ttl_packs pakeliu cigareciu uz  $ttl_price eur";
 
-for ($i = 1; $i <= $kates; $i++) {
-	print "<br/>Kačių ciklas $i <br/>";
-    for ($b = 1; $b <= $sunys; $b++) {
-		print "šunų ciklas $b <br/>";
-//		for ($j = 1; $j <= $peles; $j++) {
-//			print "pelių ciklas $j <br/>";
-		}
-        $pavyko = rand(0, 1);
-        print "randomizuotas skaicius $pavyko <br />";
-        if ($pavyko) {
-            $katasuniai++;
-            break;
-        }
-    }
-
-$h2_text = "Dalyvavo $kates kates ir $sunys sunys";
-$h3_text = "Katasuniu iseiga: $katasuniai";
 ?>
 <html>
     <head>
         <meta charset="utf-8">
     </head>
     <body>
-        <h1>Katasuniu iseiga</h1>
-        <h2><?php print $h2_text; ?></h2>
-        <h3><?php print $h3_text; ?></h3>
+        <h1>Mano dumu skaiciuokle</h1>
+        <h2><?php print $text; ?></h2>
+
+
+
     </body>
 </html>
+
