d716839b6b843f5c18c13725ed62744c8cd01e39
KamileGuogyte <kamile.guogyte@gmail.com>
2019-09-17 03:37:30 +0300

    Darbo dienu nerukymas for

--- index.php
+++ index.php
@@ -1,50 +1,83 @@
-<?php
 
+<?php
 $days = 365;
-$cigs_mon_fri = rand(3, 4);
-$cigs_sat = rand(10, 20);
-$cigs_sun = rand(1, 3);
-$pack_price = 3.50;
 
 $count_ttl = 0;
-$price_ttl = 0;
 
-for ($i = 0; $i < $days; $i++) {
-//    print $i;
-    $savaites_diena = date('N', strtotime("+$i days"));
-//print $savaites_diena<br>"
+$cigs_saved = 0;
 
-    if ($savaites_diena < 6) {
-        $cigs_mon_fri = rand(3, 4);
-        $count_ttl += $cigs_mon_fri;
-//        print "$savaites_diena-ieni surukiau $cigs_mon_fri<br>";
-    } elseif ($savaites_diena == 6) {
-        $cigs_sat = rand(10, 20);
+
+
+for ($i = 1; $i <= $days; $i++) {
+
+    $cigs_sun = rand(5, 6);
+
+    $cigs_sat = rand(10, 20);
+
+    $cigs_mon_fri = rand(3, 4);
+
+    $time_per_cig = rand(3, 7);
+
+    $pack_price = rand(3.4, 3.6);
+
+
+
+    $loop_date = date('N', strtotime("+$i day"));
+
+    if ($loop_date == 7) {
+
+        $count_ttl += $cigs_sun;
+    } elseif ($loop_date == 6) {
+
         $count_ttl += $cigs_sat;
-//        print "$savaites_diena-ieni surukiau $cigs_sat<br>";
-    } else
-        $cigs_sun = rand(1, 3);
-    $count_ttl += $cigs_sun;
-//    print "$savaites_diena-ieni surukiau $cigs_sun<br>";
+    } elseif ($loop_date == 5) {
+
+        $count_ttl += $cigs_mon_fri;
+    } else {
+
+        $cigs_saved += $cigs_mon_fri;
+    }
 }
-print ($count_ttl/20) . "<br>";
-$ttl_packs = ceil($count_ttl/20);
-print "$ttl_packs";
-$ttl_price = $ttl_packs * $pack_price;
-print "$ttl_price";
-$text = "Per $days surukysiu $ttl_packs pakeliu cigareciu uz  $ttl_price eur";
 
+
+
+$time_total = $count_ttl * $time_per_cig / 60;
+
+$time_hours = intval($time_total);
+
+$time_mins = intval(($time_total - $time_hours) * 60);
+
+
+
+$price_ttl = ($count_ttl / 20) * $pack_price;
+
+$price_mon_thu = ($cigs_saved / 20) * $pack_price;
+
+
+
+$h2_text = "Per $days dienas, surukysiu $count_ttl cigarečių už $price_ttl eur.";
+
+$h3_text = "Nerukydamas darbo dienomis, sutaupyčiau $price_mon_thu eur.";
 ?>
+
 <html>
+
     <head>
-        <meta charset="utf-8">
+
+        <meta charset="UTF-8">
+
+        <title>Užduotis #6 - darbo dienų sutaupymas</title>
+
     </head>
+
     <body>
-        <h1>Mano dumu skaiciuokle</h1>
-        <h2><?php print $text; ?></h2>
 
+        <h1>Mano dūmų skaičiuoklė</h1>
 
+        <h2><?php print $h2_text; ?></h2>
+
+        <h3><?php print $h3_text; ?></h3>
 
     </body>
-</html>
 
+</html>
