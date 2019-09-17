ef65975843625f5915d08affeb8cf00ac9b2a2c5
KamileGuogyte <kamile.guogyte@gmail.com>
2019-09-17 09:29:01 +0300

    Nerukymas taisytas for

--- index.php
+++ index.php
@@ -1,110 +1,40 @@
-
-
 <?php
 $days = 365;
-
 $count_ttl = 0;
+$count_ttl_work_day = 0;
+$pack_price = 3.5;
 
-$cigs_saved = 0;
-
-
-
-for ($i = 1; $i <= $days; $i++) {
-
-    $cigs_sun = rand(5, 6);
-
-    $cigs_sat = rand(10, 20);
-
-    $cigs_mon_fri = rand(3, 4);
-
-    $time_per_cig = rand(3, 7);
-
-    $pack_price = rand(3.4, 3.6);
-
-
-
-    $loop_date = date('N', strtotime("+$i day"));
-
-    if ($loop_date == 7) {
-
-        $count_ttl += $cigs_sun;
-    } elseif ($loop_date == 6) {
-
-        $count_ttl += $cigs_sat;
-    } elseif ($loop_date == 5) {
-
-        $count_ttl += $cigs_mon_fri;
+for ($i = 0; $i < $days; $i++) {
+	$savaites_diena = date('N', strtotime("+$i days"));	
+	print "$savaites_diena";
+	if ($savaites_diena <= 5) {
+        $cigs_mon_fri = rand(3, 4);
+		$count_ttl_work_day += $cigs_mon_fri;
+    } elseif ($savaites_diena == 6) {
+		$cigs_sat = rand(10, 20);
+		$count_ttl += $cigs_sat;
     } else {
-
-        $cigs_saved += $cigs_mon_fri;
+		$cigs_sun = rand(1, 3);
+		$count_ttl += $cigs_sun;
     }
 }
 
+$total_packs = ceil($count_ttl / 20);
+$price_ttl = $total_packs * $pack_price;
+$rezultatas = "Per $days dienas, surūkysiu $count_ttl cigarečių už $price_ttl eurų.";
 
 
-$time_total = $count_ttl * $time_per_cig / 60;
-
-$time_hours = intval($time_total);
-
-$time_mins = intval(($time_total - $time_hours) * 60);
-
-
-
-$price_ttl = ($count_ttl / 20) * $pack_price;
-
-$price_mon_thu = ($cigs_saved / 20) * $pack_price;
-
-$pack_count = intval($count_ttl / 20) + 1;
-
-
-
-$h2_text = "Per $days dienas, surukysiu $count_ttl cigarečių už $price_ttl eur.";
-
-$h3_text = "Nerukydamas darbo dienomis, sutaupyčiau $price_mon_thu eur.";
+        
+$savings = "Nerūkydamas darbo dienomis sutaupyčiau $count_ttl_work_day eurų";
 ?>
-
 <html>
-
     <head>
-
         <meta charset="UTF-8">
-
-        <title>Užduotis EKSTRA</title>
-
-        <style>
-
-            .pack {
-
-                display: inline-block;
-
-                background-image: url(https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Fcdn2.bigcommerce.com%2Fserver4700%2Ff4505%2Fproducts%2F35790%2Fimages%2F184452%2Fhaz_flashcigarette-full__46511.1547602571.120.120.png%3Fc%3D2&f=1&nofb=1);
-
-                background-size: contain;
-
-                height: 60px; 
-
-                width: 60px;
-
-                border: 1px solid #000000;
-
-                margin: 2px;
-
-            }
-
-        </style>
-
+        <title>Cigaretes Užduotis #5</title>
     </head>
-
     <body>
-
         <h1>Mano dūmų skaičiuoklė</h1>
-
-<?php for ($i = 0; $i < $pack_count; $i++): ?>
-
-            <div class="pack"></div>
-
-<?php endfor; ?>
-
+        <h2><?php print $rezultatas; ?></h2>
+         <h3><?php print $savings; ?></h3>
     </body>
-
-</html>
+</html> 
