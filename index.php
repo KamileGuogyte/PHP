b90eb584d2b1820a5710e8ef13607e6b44a4f43b
KamileGuogyte <kamile.guogyte@gmail.com>
2019-09-19 16:40:45 +0300

    arrays, foreach, simple

--- index.php
+++ index.php
@@ -1,20 +1,68 @@
-
 <?php
+$week_days = [
+    'Monday' => 'darbo diena',
+    'Tuesday' => 'darbo diena',
+    'Wednesday' => 'darbo diena',
+    'Thurstay' => 'darbo diena',
+    'Friday' => 'darbo diena',
+    'Saturday' => 'savaitgalis',
+    'Sunday' => 'savaitgalis',
+];
+//
+//$week_days[4] = 'Black';
+//var_dump($week_days);
+//
+//unset($week_days[4]);    
+//var_dump($week_days);
+//
+//foreach ($week_days as $index_idx => $days) {
+// if ($index_idx == 'Friday');{
+//     $days = 'Friday yra gera diena';
+// }
+//};
+//var_dump($days);
 
-$array = [];
-for ($i = 1; $i < 7; $i++) {
-    $array[] = date('l', strtotime("+ $i days")); 
-}
-    var_dump($array);
 
+foreach ($week_days as $index_idx => $days) {
+    if ($days == 'darbo diena') {
+        $week_days[$index_idx] = [
+            'Keliamės 6:00',
+            'Sporto klubas',
+            'Rytinis kamštis',
+            'Code Academy',
+            'Vakarinis kamštis',
+            'Netflix'
+        ];
+    }
+};
+
+var_dump($week_days);
 ?>
-
 <html>
     <head>
         <meta charset="UTF-8">
-        <title></title>
+        <title>Užduotis</title>
     </head>
     <body>
-        <h2><?php  ?></h2>
+        <h1></h1>
+        <ul>
+        </ul>
     </body>
-</html> 
+</html>
+
+
+
+
+
+<!--
+//foreach ($mano_atmintis as $rezultatas){
+//     if (!in_array($rezultatas, $tuscias_masyvas)) {
+//            $tuscias_masyvas[] = $rezultatas;
+//        } else {
+//            print $rezultatas. '<br />';   
+-->
+
+
+
+
+
