c564e0beca0be67516b346d8211b817f33b68a07
KamileGuogyte <kamile.guogyte@gmail.com>
2019-09-27 15:35:10 +0300

    Reference 3 užduotis

--- index.php
+++ index.php
@@ -1,14 +1,16 @@
 <?php
 
-$x = 0;
-$b = &$x; 
+$roll_joints = true;
+
+$joint1 = &$roll_joints;
+$joint2 = &$joint1;
+$joint3 = &$joint2;
+
+print $joint1;
+print $joint2;
+print $joint3;
 
 
-unset($b);
-
-$b = 1; 
-
-print $x;
 
 
 
