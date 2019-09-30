01313dbadf9b84431ec29fc15c65a8a9e43788be
KamileGuogyte <kamile.guogyte@gmail.com>
2019-09-30 12:43:28 +0300

    Forms didinam skaičių

--- index.php
+++ index.php
@@ -1,32 +1,27 @@
 <?php
 
 var_dump($_POST);
+$value = 0;
 
-$ats = 0;
-
-function square($x) {
-    return $x ** 2;    
+if (isset($_POST['enter'])) {
+    $value = $_POST['enter'];
+    $value++;
 }
 
-if (isset($_POST['enter'])){
-    $ats = square($_POST['skaicius']);
-}
 
-$text = "Atsakymas: $ats";
+
 
 ?>
 <html>
     <head>
-        <meta charset="UTF-8"
+        <meta charset="UTF-8">
         <title>Forms</title>
     </head> 
     <body>
         <form method="POST">
-            <span>Pakelti kvadratu:</span>
-            <input type="number" name="skaicius" required/>
-            <input type="submit" name="enter"/>
+            <input type="submit" name="enter" value="<?php print $value; ?>"/>
         </form>
-        <h1><?php print $text; ?></h1>
+ 
     </body>
 </html>
 
