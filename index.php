7479b0f16d772880c93df98691de1ef9889d547e
KamileGuogyte <kamile.guogyte@gmail.com>
2019-09-30 10:48:56 +0300

    Functions

--- index.php
+++ index.php
@@ -1,17 +1,30 @@
 <?php
 
+$array = ['a', 'c', 't', 't', 'n'];
 
-$sheep = ['blee'];
-
-for ($x = 0; $x < 4; $x++){
-    var_dump($x);
-   $sheep[] = &$sheep[$x];
-   
+function count_values($array, $value) {
+    $counter = 0;
+    foreach ($array as $ride) {
+        if ($ride === $value) {
+            $counter++;
+        }  
+    }
+    return $counter;
 }
 
-$sheep[0] = 'kitaip';
 
- var_dump($sheep);
+$array = ['a', 'c', 't', 't', 'n'];
+
+function change_values(&$array, $val_from, $val_to) {
+    foreach ($array as &$ride) {
+        if ($ride === $val_from) {
+            $ride = $val_to;
+        }  
+    }
+}
+
+change_values($array, 't', 'T');
+var_dump($array);
 
 
 ?>
