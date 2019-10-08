40e6e63d2446d6346cef7ddffac3d00b4e505ac8
KamileGuogyte <kamile.guogyte@gmail.com>
2019-10-08 09:18:39 +0300

    Funkcijos (pradmenys)

--- index.php
+++ index.php
@@ -1,37 +1,84 @@
 <?php
 
-$array = [
-    'participants' => [
-        [
-            'name' => 'Juozas',
-            'surname' => 'Juozaitis',
-            'age' => 86,
-        ],
-        [
-            'name' => 'Dalia',
-            'surname' => 'Zemkalnyte',
-            'age' => 28,
-        ],
-        [
-            'name' => 'Mantas',
-            'surname' => 'Britkus',
-            'age' => 41,
-        ]
-    ]
-];
 
 
-$new_array = [];
 
-foreach ($array['participants'] as $value) {
-   $new_array[] = $value['age'];
+  function add_one($var) {
+      return ++$var;
+  }
+  
+  print add_one(1)
+  
+  
+  
+  $x = 4
+          $y = 2
+  function add_some($x, $y) { deklaracija
+      return ($x + $y)
+  }
+ 
+  print add_sum ($x, $y)
+          
+   
+          
+    $x = 4
+          $y = 2
+  function add_mode($x, $y) { deklaracija
+      return $x
+  }
+ 
+  print add_mode ($x, $y)      (vienas skaicius)
+          
+          
+     
+              
+    $x = 4
+          $y = 2
+  function add_mode(&$x, $y) { deklaracija
+      return $x + $y
+  }
+ 
+  print add_mode ($x, $y)   bus 6    
+          
+          
+      $x = 4
+          $y = 2
+  function add_mode(&$x, $y) { deklaracija
+      return $x + $y
+  }
+ 
+  print $x   bus 4       
+     
+          
+         $x = 4
+          $y = 2
+  function add_mode(&$x, &$y) { deklaracija
+       $x= $x + $y
+        $y= $y + $x
+          
+  }
+ 
+  add_mode($x, $y);
+  print "$x and $y"         
+          
+  ats: 6 and 8     
       
-}
+      
+      
+          $x = 4
+          $y = 2
+  function add_mode(&$x, &$y) { deklaracija
+       $x += $y
+        $y +=$x
+          return "$x ans $y"
+          
+  }
+ 
 
-  var_dump($new_array);
-
-
-
+  print ad_mode ($x ,$y )        
+          
+  ats: 6 and 8      
+      
 ?>
 <html>
     <head>
